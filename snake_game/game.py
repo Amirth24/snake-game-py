022d52a3cb88a22fa3137b61cb924ea4e66018aa
Amirthanathan R <amirth300324@gmail.com>
2023-04-28 20:16:02 +0530

    Creating World Object in Game

--- snake_game/game.py
+++ snake_game/game.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 import pygame as pg
 
+from .world import World
+
 @dataclass
 class GameConfig:
     win_dim : tuple[int, int] = (600, 480)
@@ -8,6 +10,10 @@ class GameConfig:
     # debug settings
     dbg_font_size:int = 14
     dbg_text_wrap: int = 30
+
+    # World Settings
+    world_cell_size: int = 10
+
 
 class Game:
     def __init__(self) -> None:
@@ -22,6 +28,16 @@ class Game:
         self.dbg_font = pg.font.SysFont('Verdana', self.config.dbg_font_size)
         self.dbg_rect = pg.rect.Rect(0,0,self.config.dbg_text_wrap, self.config.dbg_font_size*10)
 
+
+        # Testing World
+
+        self.world = World(self.config.win_dim, self.config.world_cell_size, self.surface)
+
+
+        print('World Width and Height',self.world.width, self.world.height)
+
+        self.world.rects = [(0,0), (5,7), (10, 10)]
+
     def update(self) -> None:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -31,6 +47,7 @@ class Game:
 
     def draw(self) -> None:
         self.surface.fill(0)
+        self.world.draw()
 
 
     def disp_dbg(self) -> None:
